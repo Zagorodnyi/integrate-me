1e1e2cee9a350cd7862f8dd5c4faecf36cf663ff
Viacheslav Zahorodnyi <zagorodnyi.me@gmail.com>
2023-02-26 22:34:23 +0200

    Reduce bundle size

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   sourcemap: true,
   dts: true,
   format: ['cjs', 'esm'],
+  clean: true,
+  minify: true,
   noExternal: ['cli-color', 'cli-spinners', 'zod'],
   entry: {
     index: 'src/index.ts',
