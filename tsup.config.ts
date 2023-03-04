bc37901b1bc255c80affffa19bb7811898e0a8f8
Viacheslav Zahorodnyi <zagorodnyi.me@gmail.com>
2023-03-04 22:10:45 +0200

    Reduce dist size

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  sourcemap: true,
   dts: true,
   format: ['cjs', 'esm'],
   clean: true,
   minify: true,
+  target: 'node12',
+  treeshake: true,
   noExternal: ['cli-color', 'cli-spinners', 'zod'],
   entry: {
     index: 'src/index.ts',
